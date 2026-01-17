73fb54746b98112f5274ac413946c85414ffd993
tarae1500 <maythe@balleza.ws>
2026-01-17 09:53:46 -0600

    comit e flask

---
+++ app.py
@@ -0,0 +1,87 @@
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.secret_key = 'clave secreta'
+
+
+@app.route('/')
+def index():
+    title = "IDGS804 -Intro Flask"
+    listado =['Juan', 'Ana', 'Pedro', 'Luisa']
+
+    return render_template('index.html', title=title, listado=listado)
+
+
+@app.route('/saludo1')
+def saludo1():
+    return render_template('saludo1.html')
+
+@app.route('/saludo2')
+def saludo2():
+    return render_template('saludo2.html')
+
+
+@app.route("/hola")
+def func():
+    return 'Hola, Mundo --Hola!'
+
+
+@app.route("/user/<string:user>")
+def user(user):
+    return f'Hola, {user}!'
+
+@app.route("/numero/<int:n>")
+def numero(n):
+    return f'<h1>Número: {n}<h1>'
+
+@app.route("/user/<int:id>/<string:username>")
+def username(id,username):
+    return f"<h1>¡Hola, {username}! Tu ID es: {id}"
+
+
+@app.route("/suma/<float:n1>/<float:n2>")
+def suma(n1,n2):
+    return f"<h1>La suma es: {n1+n2}</h1>"
+
+
+@app.route("/default")
+@app.route("/default/<string:parm>")
+def func2(param="juan"):
+    return f"<h1>¡Hola, {param}!</h1>"
+
+
+app.route("/operas")
+def operas():
+    return '''
+    <form>
+    <label for="name>Name:</label>
+    <input type="text" id="name" name="name" required>
+    </br>
+    <label for="name">apaterno:</label>
+    <input type="text" id="name" name="name" required>
+    </br>
+    <input type="submit" value="Submit">
+    </form>
+    '''
+
+@app.route("/OperasBas")
+def operasbas():
+    return render_template('operasBas.html')
+
+@app.route("/resultado", methods=['GET', 'POST'])
+def result1():
+    n1=request.form.get('num1')
+    n2=request.form.get('num2')
+    return f"<h1>La suma es: {float(n1)+float(n2)}</h1>"
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+    
